ebd872aa8df623c7f0e659744d13de91b4e874bd
gabriel <gabrielzuleta.m@gmail.com>
2021-04-15 14:59:11 -0400

    Angular project update

--- project1/src/app/app.module.ts
+++ project1/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ProductComponent} from './components/product-component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
---
+++ project1/src/app/components/product-component.ts
@@ -0,0 +1,55 @@
+import {
+    Component,
+    Input,
+    Output,
+    EventEmitter,
+    OnChanges,
+    SimpleChanges,
+    OnInit,
+    DoCheck,
+    OnDestroy
+} from '@angular/core';
+import { Product } from '../product-model';
+
+@Component({
+    selector: 'app-product',
+    templateUrl: './product-component.html',
+    styleUrls: ['./product-component.scss']
+})
+export class ProductComponent implements OnInit, DoCheck, OnDestroy {
+
+    @Input() product: Product;
+    @Output() productClicked: EventEmitter<any> = new EventEmitter();
+
+    constructor() {
+        console.log('1. constructor');
+    }
+
+    // tslint:disable-next-line: typedef
+    // ngOnChanges(changes: SimpleChanges) {
+    //    console.log('2. ngOnChanges');
+    //    console.log(changes);
+    // }
+
+    // tslint:disable-next-line: typedef
+    ngOnInit() {
+        console.log('3. ngOnInit');
+    }
+
+    // tslint:disable-next-line: typedef
+    ngDoCheck() {
+        console.log('4. ngDoCheck');
+    }
+
+    // tslint:disable-next-line: typedef
+    ngOnDestroy() {
+        console.log('5. ngOnDestroy');
+    }
+
+    // tslint:disable-next-line: typedef
+    addCart() {
+        console.log('Add to cart');
+        this.productClicked.emit(this.product.id);
+    }
+
+}
